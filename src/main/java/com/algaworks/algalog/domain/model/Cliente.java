e0fec60dd5aaa8d692a9b83648399ba48ab56bce
MarceloSLeal <marcelosleal@outlook.com>
2023-03-16 16:37:49 -0300

    Adicionado um EntityManager no controler com injeção de dependência@PersistenceContext que irá retornar no mapeamento /clientes a lista de clientes na tabela clientes, pesquisando com jpql para pegar o objeto

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,26 @@
 package com.algaworks.algalog.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
